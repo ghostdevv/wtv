2686a8437aacd2685c075edc4597c6e0ece007de
GHOST <ghostdevbusiness@gmail.com>
2025-03-18 22:47:40 +0000

    feat: add nicer border

--- src/routes/dialog/+layout.svelte
+++ src/routes/dialog/+layout.svelte
@@ -9,3 +9,11 @@
 </script>
 
 {@render children()}
+
+<style lang="scss">
+	:global(html) {
+		border: 2px solid var(--background-tertiary);
+		box-shadow: 0px 0px 18px 2px var(--background-tertiary) inset;
+		border-radius: 12px;
+	}
+</style>
